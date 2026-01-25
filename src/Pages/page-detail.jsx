1dc46262bd9c84e481903b4fd1d01a80d408fb1c
Hiura <hiura0012@gmail.com>
2026-01-25 08:22:51 +0700

    Update page-detail.jsx

--- src/Pages/page-detail.jsx
+++ src/Pages/page-detail.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import DetailComic from '../components/Detail-Comic/DetailComic'
+import SEO from '../components/SEO'
 
 const PageDetail = () => {
-  return (
-    <DetailComic/>
-  )
+    return (
+        <>
+            <SEO 
+                title="Detail Komik"
+                description="Baca komik favorit Anda dengan koleksi lengkap chapter"
+            />
+            <DetailComic />
+        </>
+    )
 }
 
 export default PageDetail
-  
